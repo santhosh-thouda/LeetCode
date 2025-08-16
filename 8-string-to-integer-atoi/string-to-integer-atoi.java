5b39071d0bdbdcd0bf61afdd2ddbffd60a1b35ce
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-08-16 23:16:34 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (47.18%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,22 +1,21 @@
 class Solution {
     public int myAtoi(String s) {
         String s1 = s.trim();
-        int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;
         int i = 0;
         int sign = 1;
         long num = 0;
         int n = s1.length();
 
         if(s1.isEmpty())return 0;
+
         if(s1.charAt(i) == '-' || s1.charAt(i) == '+'){
             sign = (s1.charAt(i) == '-') ? -1 : 1;
             i++;
         }
         while(i < n && Character.isDigit(s1.charAt(i))){
             num = num * 10 + (s1.charAt(i) - '0'); // in ascii code '0'=48
-            if(num * sign > max)return max;
-            if(num * sign < min)return min;
+            if(num * sign > Integer.MAX_VALUE)return Integer.MAX_VALUE;
+            if(num * sign < Integer.MIN_VALUE)return Integer.MIN_VALUE;
             i++;
         } 
         return (int)(sign*num);
