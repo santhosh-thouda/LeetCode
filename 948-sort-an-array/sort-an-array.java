2bd2a2904c35f9c039496b39883fd02c924fbf38
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-08-27 23:37:39 +0530

    Time: 27 ms (54.79%) | Memory: 56.2 MB (52.16%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,39 +1,53 @@
+import java.util.Random;
+
 class Solution {
     public int[] sortArray(int[] nums) {
         mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-
-    private void mergeSort(int[] arr, int left, int right) {
-        if (left >= right) return;
-
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+    private void mergeSort(int []nums, int left, int right){
+        if(left < right){
+            int mid = left + (right- left)/2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
+        }
     }
-
-    private void merge(int[] arr, int left, int mid, int right) {
+    private void merge(int nums[], int left, int mid, int right){
         int n1 = mid - left + 1;
         int n2 = right - mid;
 
-        int[] L = new int[n1];
-        int[] R = new int[n2];
+        int L[] = new int[n1];
+        int R[] = new int[n2];
 
-        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
-        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
-
-        int i = 0, j = 0, k = left;
-
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k++] = L[i++];
-            } else {
-                arr[k++] = R[j++];
-            }
+        for(int i = 0; i<n1; i++){
+            L[i] = nums[left + i];
+        }
+        for(int j = 0; j<n2; j++){
+            R[j] = nums[mid+1 + j];
         }
 
-        while (i < n1) arr[k++] = L[i++];
-        while (j < n2) arr[k++] = R[j++];
+        int i = 0, j = 0, k = left;
+        while(i < n1 && j < n2){
+            if(L[i] < R[j]){
+                nums[k] = L[i];
+                i++;
+            }
+            else{
+                nums[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < n1){
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+        while(j < n2){
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
     }
 }
