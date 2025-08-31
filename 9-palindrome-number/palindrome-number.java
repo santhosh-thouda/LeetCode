48ce41ff2ec9b0001a4776ea7e6b902f071fe740
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-08-31 20:09:07 +0530

    Time: 5 ms (65.56%) | Memory: 43.8 MB (89.02%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -3,6 +3,7 @@ class Solution {
         int temp = n;
         int rev = 0;
         while(n > 0){
+            //instead of creating int ld = n % 10, below were are just doing without ld seperately
             rev = rev * 10 + n % 10;
             n = n / 10;
         }
