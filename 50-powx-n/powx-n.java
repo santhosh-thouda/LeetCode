f8696dc4060f4e7594b7fc639e4e6ba5abf60aa5
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-09-23 18:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (11.05%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,7 +2,7 @@ class Solution {
     public double myPow(double x, int n) {
         if(n < 0){
             x = 1/x;
-            n = -(n);
+            n = -(-n);
         }
         return fastPow(x, n);
     }
@@ -10,8 +10,7 @@ class Solution {
     public double fastPow(double x, int n){
         if(n == 0)return 1;
         double half = fastPow(x, n/2);
-
-        if(n % 2 == 0)return half*half;
-        else return x*half*half;
+        if(n % 2 == 0)return half * half;
+        else return x * half * half;
     }
 }
