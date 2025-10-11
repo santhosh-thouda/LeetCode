5de7369be69e10fc9ddc1d3db90af50d70771e28
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-10-11 18:04:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (84.99%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,21 +1,17 @@
 class Solution {
     static int mod = 1000000007;
     public int countGoodNumbers(long n) {
-        
-        long evenCount = (n + 1)/2; //ceil value
-        long oddCount = (n/2); // floor value
+        long evenCount = (n + 1)/2;
+        long oddCount = n/2;
 
-         // (5^evenCount * 4^oddCount) % MOD
         long res = (fastPow(5, evenCount, mod) * fastPow(4, oddCount, mod)) % mod;
-        return (int)(res);
+        return (int)res;
     }
-    
-    public long fastPow(long x, long n, int mod){
+    private static long fastPow(long x, long n, int mod){
         if(n == 0)return 1;
-        
         long half = fastPow(x, n/2, mod);
         long result = (half * half) % mod;
-        
+
         if(n % 2 != 0){
             result = (result * x) % mod;
         }
