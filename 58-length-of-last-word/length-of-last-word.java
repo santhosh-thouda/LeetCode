8072817a950abf09ac13033ec6231ed0c1f3484e
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-12-25 15:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (12.53%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,11 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String [] split = s.split(" ");
-        for(int i = split.length - 1; i>=0; i--){
-            if(i == split.length - 1){
-                return split[i].length();
-            }
+        int len = 0;
+
+        int i = s.length()-1;
+        while(i >= 0 && s.charAt(i) == ' '){
+            i--;
         }
-        return 0;
+
+        while(i >= 0 && s.charAt(i) != ' '){
+            len++;
+            i--;
+        }
+        return len;
     }
 }
