724340bd036552b5e31cce302a69a1d3ce371a2c
Santhosh Thouda <91472196+santhosh-thouda@users.noreply.github.com>
2025-12-30 16:04:18 +0530

    Time: 18 ms (14.71%) | Memory: 74.2 MB (23.05%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,22 +1,14 @@
-// import java.util.HashSet;
-import java.util.Arrays;
-
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean[] seen = new boolean[n+1];
+        int smallestNum = 1;
 
-        for(int i : nums){
-            if(i > 0 && i <= n){
-                seen[i] = true;
+        Arrays.sort(nums);
+
+        for(int num : nums){
+            if(num == smallestNum){
+                smallestNum++;
             }
         }
-
-        for(int i = 1; i<=n; i++){
-            if(!seen[i]){
-                return i;
-            }
-        }
-        return n + 1;
+        return smallestNum;
     }
 }
